551275ceb62d6d956e765f26717b76b38a4e46ee
Poultergeist <vlogatskiy3@gmail.com>
2024-10-11 22:08:44 +0300

    created new page, fixed adaptive, create modal, some else fixes

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const openModalBtn = document.getElementById('open-modal');
+const modal = document.getElementById('modal');
+const closeModalBtn = document.getElementById('close-modal');
+
+openModalBtn.addEventListener('click', toggleModal);
+closeModalBtn.addEventListener('click', toggleModal);
+
+function toggleModal() {
+  modal.classList.toggle('modal-visible');
+}
+
+const termsBtn = document.getElementById('terms');
+
+termsBtn.addEventListener('click', function() {
+  this.classList.toggle('terms-agree');
+})
